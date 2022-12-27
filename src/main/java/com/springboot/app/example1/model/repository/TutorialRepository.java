e87be606b9a49385381a072e3fc2399d2c728e14
shahidul <shahidul.hasan@itcbd.com>
2022-12-27 23:47:13 +0600

    first commit

---
+++ src/main/java/com/springboot/app/example1/model/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.app.example1.model.repository;
+
+import java.util.List;
+
+import com.springboot.app.example1.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
